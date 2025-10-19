26928ca52931694f567dc4c6f9f470b26277c392
wilton <carvajaljavier60@gmail.com>
2025-10-19 18:30:38 -0500

    fix: Restaurar index.html y preparacion para Netlify

---
+++ sistema-miscelanea/src/pages/Reportes.jsx
@@ -0,0 +1,15 @@
+// src/pages/Reportes.jsx
+
+import React from 'react';
+
+function Reportes() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Reportes y Historial de Ventas</h1>
+      <p>Aquí irá la tabla de ventas pasadas de MongoDB Atlas.</p>
+    </div>
+  );
+}
+
+// ¡Esta línea es la que faltaba o estaba mal!
+export default Reportes;
