371c9765c34d1d43831c5c1e1560fd9b1f15e3c3
wilton <carvajaljavier60@gmail.com>
2025-12-09 22:30:22 -0500

    intento2

--- sistema-miscelanea/src/pages/Reportes.jsx
+++ sistema-miscelanea/src/pages/Reportes.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-import FacturaImprimible from '../components/FacturaImprimible';
+import '../components/FacturaImprimible';
 
 
 function safeDateString(d) {
